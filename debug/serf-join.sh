6cd0b7eab00c6b9f77eb5a3d3fe0827ecc2680d9
Zaephor <Zaephor@users.noreply.github.com>
2018-06-28 22:18:24 -0700

    Improving debug join script

--- debug/serf-join.sh
+++ debug/serf-join.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-serf agent -discover=$(arp -n $(ip route|awk '/default/ { print $3 }') | awk '!/Address/{print $3}' | tr -d ':')
+iface=$(ip route | awk '/default/{print $5}')
+discover=$(arp -n $(ip route|awk '/default/ { print $3 }') | awk '!/Address/{print $3}' | tr -d ':')
+advertise=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
+serf agent -discover=${discover} -iface=${iface} -advertise=${advertise}
